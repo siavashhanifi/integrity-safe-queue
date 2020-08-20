004d8acd745d4fe330f58b30ef011444cef9a06b
Siavash Hanifi <siavash.hanifi@gmail.com>
2020-08-20 14:46:29 +0200

    Add CssBaseline (css-reset from material-ui) to App

--- react_front_end/src/App.js
+++ react_front_end/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <React.Fragment>
+            <CssBaseline /> 
+    </React.Fragment>
   );
 }
 
