c5a54efcdfab80aaf257415a3a8689af8f082a80
Siavash Hanifi <siavash.hanifi@gmail.com>
2020-08-20 14:34:24 +0200

    Add dependency: npm install fontsource-roboto

--- react_front_end/src/index.js
+++ react_front_end/src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import store from './redux/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import 'fontsource-roboto';
 
 ReactDOM.render(
   <React.StrictMode>
