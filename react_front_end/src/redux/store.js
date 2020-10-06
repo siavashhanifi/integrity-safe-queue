f1e15c2fa41cf8a1b09d7a764059bece33cf592d
Siavash Hanifi <siavash.hanifi@gmail.com>
2020-10-06 10:20:08 +0200

    create and link rootReducer to store (redux)

--- react_front_end/src/redux/store.js
+++ react_front_end/src/redux/store.js
@@ -1,6 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-
+import rootReducer from './reducers/rootReducer';
 export default configureStore({
-  reducer: {
-  },
+  reducer: { rootReducer },
 });
