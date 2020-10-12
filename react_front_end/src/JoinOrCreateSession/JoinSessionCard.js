b329ef3d4655da9eb5a1e934862889cc3d915007
Siavash Hanifi <siavash.hanifi@gmail.com>
2020-10-12 16:04:11 +0200

    add student-supervisor radioboxes and connected supervisor password textfield

--- react_front_end/src/JoinOrCreateSession/JoinSessionCard.js
+++ react_front_end/src/JoinOrCreateSession/JoinSessionCard.js
@@ -1,55 +1,89 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { CardContent, TextField, Typography, Button, Grid, Card } from '@material-ui/core';
-
-
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  CardContent,
+  TextField,
+  Typography,
+  Button,
+  Grid,
+  Card,
+} from "@material-ui/core";
+import RadioButtons from './RadioButtons';
 
 const useStyles = makeStyles({
-    cardContent: {
-        padding: '16px',
-        "&:last-child": {
-            paddingBottom: '16px'
-        }
+  cardContent: {
+    padding: "16px",
+    "&:last-child": {
+      paddingBottom: "16px",
     },
+  },
 });
 
-
 export default function JoinSessionCard() {
-    const classes = useStyles();
-    const [sessionId, setSessionId] = React.useState('');
-    const handleChangedSessionId = (event) => {
-        setSessionId(event.target.value);
-    };
-    return(
-        <Card >
-        <CardContent className={classes.cardContent}>
-            <Grid
-                container
-                spacing={2}
-                direction="column"
-                alignItems="center"
-                justify="center"
-            >
+  const classes = useStyles();
+  const [role, setRole] = React.useState('student');
 
-                <Grid item>
-                    <Typography gutterBottom variant="h5" component="h2">
-                        Join session
+  const [sessionId, setSessionId] = React.useState("");
+  const handleChangedSessionId = (event) => {
+    setSessionId(event.target.value);
+  };
+
+
+  const [password, setPassword] = React.useState("");
+  const handleChangedPassword = (event) => {
+    setPassword(event.target.value);
+  };
+  
+
+  return (
+
+    <Card>
+      <CardContent className={classes.cardContent}>
+        <Grid
+          container
+          spacing={2}
+          direction="column"
+          alignItems="center"
+          justify="center"
+        >
+          <Grid item>
+            <Typography gutterBottom variant="h5" component="h2">
+              Join session
             </Typography>
-                </Grid>
+          </Grid>
 
-                <Grid item>
-                    <TextField id="outlined-basic"
-                        label="Session id"
+          <Grid item>
+            <TextField
+              id="outlined-basic"
+              label="Session id"
+              variant="outlined"
+              value={sessionId}
+              onChange={handleChangedSessionId}
+            />
+          </Grid>
+          <Grid item>
+            <RadioButtons role={role} setRole={setRole}/>
+          </Grid>
+        
+            {role === 'supervisor' ?
+                    (<Grid item>
+                    <TextField
+                        id="outlined-basic"
+                        label="Password"
                         variant="outlined"
-                        value={sessionId}
-                        onChange={handleChangedSessionId}
-                    />
-                </Grid>
-                <Grid item>
-                    <Button color="primary" variant="contained">Join</Button>
-                </Grid>
-            </Grid>
-        </CardContent>
+                        value={password}
+                        onChange={handleChangedPassword}
+                      />
+                    </Grid>)
+            : null }
+
+          <Grid item>
+            <Button color="primary" variant="contained">
+              Join
+            </Button>
+          </Grid>
+        </Grid>
+      </CardContent>
     </Card>
-    );
+  );
 }
